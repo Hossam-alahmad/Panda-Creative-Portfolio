fc2b338386587fe477237247a29c7c5b6bf6ab6f
Hossam Alahmed <hossamahmad1998@gmail.com>
2021-12-23 10:32:21 +0200

    update codes
    update css files
    update bootstrap from 4 to 5
    delete unused files
    update html files
    update js files
    make template multiple colors

--- js/main.js
+++ js/main.js
@@ -1,156 +1,93 @@
-$(document).ready(() => {  
-    $(document).on("scroll",()=>{
-        console.log($(document).scrollTop());
-        if($(document).scrollTop() > 0){
-            $(".navbar").addClass("nav-fixed");
-        }
-        else{
-            $(".navbar").removeClass("nav-fixed");
-        }
-        if($(document).scrollTop() <= 0){
-            navMenuActive("#home");
-        }
-        if($(document).scrollTop() >= 300){
-            $("#img-container").removeClass("img-animate-left");
-            $("#content").removeClass("content-animate-right");
-        }
-        if($(document).scrollTop() > 629 && $(document).scrollTop() < 1137){
-            navMenuActive("#about");
-        }
-        if($(document).scrollTop() > 1137 && $(document).scrollTop() < 1770){
-            navMenuActive("#skills");
-        }
-        if($(document).scrollTop() >= 1600){
-            $(".service-item").removeClass("animate-scale");
-        }
-        if($(document).scrollTop() > 1770 && $(document).scrollTop() < 2326){
-            navMenuActive("#services");
-        }
-        if($(document).scrollTop() > 2326 && $(document).scrollTop() < 3026){
-            navMenuActive("#portfolio");
-        }
-        if($(document).scrollTop() > 3026){
-            navMenuActive("#contact-me");
-        }
-    });
-    $("#home").on("click",(e)=>{
-        e.preventDefault();
-        navMenuActive("#home");
-        scrollTranslate(0);
-        
-    });
-    $("#about").on("click",(e)=>{
-        navMenuActive("#about");
-        e.preventDefault();
-        scrollTranslate(629);
-        
-    });
-    $("#skills").on("click",(e)=>{
-        e.preventDefault();
-        navMenuActive("#skills")
-        scrollTranslate(1280);
-        
-    });
-    $("#services").on("click",(e)=>{
-        e.preventDefault();
-        navMenuActive("#services");
-        scrollTranslate(1860);
-        
-    });
-    $("#portfolio").on("click",(e)=>{
-        e.preventDefault();
-        navMenuActive("#portfolio");
-        scrollTranslate(2360);
-        
-    });
-    $("#contact-me").on("click",(e)=>{
-        e.preventDefault();
-        navMenuActive("#contact-me");
-        scrollTranslate(3062);
-        
-    });
-    $(".navbar #menu-bar").on("click",()=>{
-        if(!$(".navbar #menu-list").hasClass("show-menu")){
-            $(".navbar #menu-list").addClass("show-menu");
-            $("#overlay").addClass("overlay-show");
-        }
-        else{
-            $(".navbar #menu-list").removeClass("show-menu");
-            $("#overlay").removeClass("overlay-show");
-        }
-    });
-    $("#overlay").on("click",()=>{
-        $(".navbar #menu-list").removeClass("show-menu");
-        $("#overlay").removeClass("overlay-show");
-    });
-    $("#btn-video").on("click",()=>{
-        $(".show-video-screen").addClass("show-screen");
-    });
-    $("#close-btn").on("click",()=>{
-        $(".show-video-screen").removeClass("show-screen");
-    });
-    function navMenuActive($id){
-        $("#menu-list li a.active").removeClass();
-        $("#menu-list li a"+$id).addClass("active");
+window.onload = () => {
+    // Show Color Setting
+    let rootCss = document.querySelector(":root"),
+        setting = document.getElementById("setting"),
+        controls = document.querySelector(".controls"),
+        colorList = document.querySelectorAll("#colors-box li");
+
+    if (window.localStorage.key("main-color") !== null) {
+        rootCss.style.setProperty(
+            "--main-color",
+            window.localStorage.getItem("main-color")
+        );
     }
-    function scrollTranslate($scrollTopValue){
-        setTimeout(() => {
-            $i = $(document).scrollTop();
-            if($i < $scrollTopValue){
-                $interval = setInterval(() => {
-                    $i += 10;
-                    $(document).scrollTop($i);
-                    if($i >= $scrollTopValue){
-                        clearInterval($interval);
-                    }
-                },1);
-            }
-            else if($i > $scrollTopValue){
-                $interval = setInterval(() => {
-                    $i -= 10;
-                    $(document).scrollTop($i);
-                    if($i <= $scrollTopValue){
-                        clearInterval($interval);
-                    }
-                },1);
-            }
-        },1000);
-    }
-    $(".menu-list ul").on('click','li',(e)=>{
-            $array = $(".portfolio .col-lg-8 .col-lg-6");
-            $(".menu-list ul li.menu-list-active").removeClass();
-            $(e.currentTarget).addClass("menu-list-active");
-            $(".portfolio .col-lg-4").fadeOut();
-            $(".portfolio .col-lg-8 .col-lg-6").fadeOut();
-            if(e.currentTarget.innerHTML === "All"){
-                $(".portfolio .col-lg-8 .col-lg-6").fadeIn();
-                $(".portfolio .col-lg-4").fadeIn();
-            }
-            else if(e.currentTarget.innerHTML === "UI/UX"){
-                $(".portfolio .col-lg-4").fadeIn();
-                for($i =0;$i<$array.length;$i++){
-                    if($(".portfolio .col-lg-8 .col-lg-6").eq($i).attr("data-text") === "Ui/Ux"){
-                        $(".portfolio .col-lg-8 .col-lg-6").eq($i).fadeIn();
-                    }
-                }
-            }
-            else if(e.currentTarget.innerHTML === "Websites"){
-                $(".portfolio .col-lg-4").fadeIn();
-                for($i =0;$i<$array.length;$i++){
-                    if($(".portfolio .col-lg-8 .col-lg-6").eq($i).attr("data-text") === "Websites"){
-                        $(".portfolio .col-lg-8 .col-lg-6").eq($i).fadeIn();
-                    }
-                }
-            }
-            else{
-                $(".portfolio .col-lg-4").fadeIn();
-                for($i =0;$i<$array.length;$i++){
-                    if($(".portfolio .col-lg-8 .col-lg-6").eq($i).attr("data-text") === "Dashboards"){
-                        $(".portfolio .col-lg-8 .col-lg-6").eq($i).fadeIn();
-                    }
-                }
-            }
-            
+    setting.addEventListener("click", () => {
+        controls.classList.toggle("show-setting");
+    });
+    colorList.forEach((el) => {
+        el.addEventListener("click", () => {
+            window.localStorage.setItem("main-color", el.dataset.value);
+            rootCss.style.setProperty("--main-color", el.dataset.value);
+        });
+    });
+    // transmation between navigation bar
+    let navList = document.querySelectorAll("#nav-list li");
+    navList.forEach((el) => {
+        el.firstElementChild.addEventListener("click", (e) => {
+            navList.forEach((els) => {
+                els.firstElementChild.classList.remove("active");
+            });
+            el.firstElementChild.classList.add("active");
+        });
+    });
+    // Fixed header
+    let header = document.getElementById("header");
+    window.onscroll = () => {
+        if (document.documentElement.scrollTop > 90)
+            header.classList.add("fixed");
+        else header.classList.remove("fixed");
+    };
+    // this for show/hide navbar
+    let menuBar = document.getElementById("menu-bar"),
+        navbar = document.getElementById("nav-bar"),
+        overlay = document.getElementById("overlay");
+
+    menuBar.addEventListener("click", () => {
+        navbar.classList.toggle("show-menu");
+        overlay.classList.toggle("show-overlay");
     });
 
-})
+    // this for show video
+    let videoScreen = document.getElementById("video-screen"),
+        videoBtn = document.getElementById("btn-video"),
+        closeVedioBtn = document.getElementById("close-btn"),
+        video = document.getElementById("video");
+
+    videoBtn.addEventListener("click", () => {
+        videoScreen.classList.add("show-screen");
+        video.classList.add("show");
+    });
+    closeVedioBtn.addEventListener("click", () => {
+        videoScreen.classList.remove("show-screen");
+        video.classList.remove("show");
+    });
+
+    // active types of portfolio
+    let typesBtn = document.querySelectorAll(".portfolio-types ul li"),
+        portfolioItems = document.querySelectorAll(".portfolio-box > div");
+    typesBtn.forEach((el) => {
+        el.addEventListener("click", () => {
+            typesBtn.forEach((ele) => {
+                ele.classList.remove("active-type");
+            });
+            portfolioItems.forEach((pi) => {
+                pi.style.display = "none";
+                pi.classList.remove("show-portfolios");
+            });
+            el.classList.add("active-type");
+            portfolioItems.forEach((pi) => {
+                if (
+                    el.textContent === pi.dataset.text ||
+                    el.textContent === "All"
+                ) {
+                    pi.style.display = "block";
+                    setTimeout(() => {
+                        pi.classList.add("show-portfolios");
+                    }, 200);
+                } else {
+                    pi.style.display = "none";
+                }
+            });
+        });
+    });
+};
